c2a6f545db701da49fd1fafc74b691a04ea3304f
Carina Sweet <carina.sweet06@gmail.com>
2021-08-04 10:06:48 -0600

    Add competition avg age method and test

--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,4 +1,8 @@
 class Competition < ApplicationRecord
   has_many :registrations
   has_many :teams, through: :registrations
+
+  def competition_avg_age
+    teams.joins(:players).average("players.age").to_i
+  end
 end
